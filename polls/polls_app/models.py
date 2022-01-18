9c1f78fa8ea717a9b3569bd0041393a9628c36bc
solviant <driad@o2.pl>
2022-01-18 15:28:01 +0100

    Just adding some commentary to the models file in polls_app app

--- polls/polls_app/models.py
+++ polls/polls_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 
+"""Just adding some commentary to the file in order to explain it better"""
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
