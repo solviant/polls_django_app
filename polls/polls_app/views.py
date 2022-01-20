e0fe3499d085315da4b9b6244c405ac191bf8032
solviant <driad@o2.pl>
2022-01-20 16:37:42 +0100

    After Wensday

--- polls/polls_app/views.py
+++ polls/polls_app/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -15,13 +16,21 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last 5 published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last 5 published questions (not 
+        including those published in the future)."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
     
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls_app/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date_lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
